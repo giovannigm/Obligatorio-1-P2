acce3e9d983f6977454e8c510808311357fcaab8
nicocadq <nmachado.camejo@gmail.com>
2025-04-26 17:35:14 -0300

    agrega la clase con un main

---
+++ src/JuegoTriangulos.java
@@ -0,0 +1,8 @@
+public class JuegoTriangulos {
+    public static void main(String[] args) {
+        // Acá llamaremos al menú y arrancaremos el juego
+        System.out.println("Bienvenidos al juego Triángulos!");
+        
+        // Por ahora no implementamos nada más
+    }
+} 
